90e7f1f39a1e65841bf956d3e6adc018a3a2d452
StarryCoffee302 <3103120143@student.smktelkom-pwt.sch.id>
2022-11-28 22:23:33 +0700

    first commit

---
+++ router/users.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const users = require('../controllers/users')
+const router = express.Router()
+
+const usercontroller = require('../controllers/users')
+
+router.get('/users', usercontroller.index)
+
+router.get('/user/:id', usercontroller.show)
+
+router.get('/user', usercontroller.index)
+
+  router.post('/user', usercontroller.store)
+
+  router.put('/user/:id', usercontroller.update)
+
+  router.delete('/user/:id', usercontroller.delete)
+
+  module.exports = router
